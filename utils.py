54f98212c1ea1e92244780e65d340210e564b0a6
Fra <zuppif@usi.ch>
2019-01-15 23:16:35 +0100

    finished

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import torch
 
 def tensor2img(tensor, ax=plt):
     tensor = tensor.squeeze()
@@ -37,3 +38,75 @@ def subplot(images, parse=lambda x: x, rows_titles=None, cols_titles=None, title
         fig.subplots_adjust(top=0.88)
         plt.subplots_adjust(wspace=0.0, hspace=0.0)
         plt.show()
+
+
+def module2traced(module, inputs):
+    handles, modules = [], []
+
+    def trace(module, inputs, outputs):
+        modules.append(module)
+
+    def traverse(module):
+        for m in module.children():
+            traverse(m)  # recursion is love
+        is_leaf = len(list(module.children())) == 0
+        if is_leaf: handles.append(module.register_forward_hook(trace))
+
+    traverse(module)
+
+    _ = module(inputs)
+
+    [h.remove() for h in handles]
+
+    return modules
+
+def run_vis_plot_across_models(modules, input, layer_id, Vis, title,
+                               device,
+                               inputs=None,
+                               nrows=3,
+                               ncols=2,
+                               row_wise=True,
+                               parse=tensor2img,
+                               annotations=None,
+                               idx2label=None,
+                               rows_name=None,*args, **kwargs):
+    pad = 0 # in points
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols)
+    fig.suptitle(title)
+
+    for i, row in enumerate(ax):
+        try:
+            module = next(modules)
+            module.eval()
+            module = module.to(device)
+            layer = None
+            if layer_id is not None: layer = module2traced(module, input)[layer_id]
+            vis = Vis(module, device)
+            info = {}
+            if inputs is None: images, info = vis(input.clone(), layer, *args, **kwargs)
+            row_title = module.__class__.__name__
+            del module
+            torch.cuda.empty_cache()
+            if rows_name is not None: row_title = rows_name[i]
+            row[0].set_title(row_title)
+            if annotations is not None:
+                row[0].annotate(annotations[i], xy=(0, 0.5), xytext=(-row[0].yaxis.labelpad - pad, 0),
+                    xycoords=row[0].yaxis.label, textcoords='offset points',
+                    size='medium', ha='right', va='center', rotation=90)
+            for j, col in enumerate(row):
+                if inputs is None: image = images[j]
+                else: image, info = vis(inputs[j], layer, *args, **kwargs)
+                if 'prediction' in info: col.set_title(idx2label[int(info['prediction'])])
+                col.imshow(parse(image))
+                col.axis('off')
+                col.set_aspect('equal')
+        except StopIteration:
+            break
+        except:
+            row.set_title(row_title)
+            row.imshow(parse(images[0]))
+            row.axis('off')
+            row.set_aspect('equal')
+    fig.tight_layout()
+    plt.subplots_adjust(wspace=0.1, hspace=0.2)
+
